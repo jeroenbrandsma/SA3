5513505bb90ff5079682fd6452f919e1126bbdd9
Joris Schafers <imjoris@gmail.com>
2015-11-07 10:25:55 +0100

    Better script to make a pdf, called makepdf

---
+++ makepdf.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+##############################
+## PARAMETERS
+##############################
+name='report'
+outputDir='build'
+
+#Set them if supplied
+if [[ ! -z $1 ]]; then
+    name=$1;
+fi
+if [[ ! -z $2 ]]; then
+    outputDir=$2;
+fi
+
+##############################
+## Set options
+##############################
+
+mkdir ${outputDir}
+
+dateNow="date +%m-%d__%H:%M"
+outputName="${name}_$(eval $dateNow)"
+
+latexInBuildDir="-output-directory=${outputDir} -aux-directory=${outputDir}"
+latexcommand='pdflatex -output-directory=build -aux-directory=build -interaction=nonstopmode'
+mycmd="${latexcommand} $name"
+
+##############################
+## EXECUTE COMMANDS
+##############################
+$mycmd
+cp library.bib ${outputDir}
+(cd ${outputDir}; bibtex $name;);
+(cd ${outputDir}; makeglossaries $name;);
+$mycmd
+$mycmd
+(cd ${outputDir}; cp "${name}.pdf" "${outputName}.pdf");
